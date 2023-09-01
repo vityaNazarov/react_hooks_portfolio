181a9d1bfafb6fa07bb3a3f95e608c15c0bed620
Viktor <108054291+vityaNazarov@users.noreply.github.com>
2023-09-01 18:50:14 +0300

    add some

--- src/helpers/ProjectsList.js
+++ src/helpers/ProjectsList.js
@@ -1,63 +1,95 @@
-import project01 from "./../img/projects/01.png";
-import project01Big from "./../img/projects/01-big.png";
+import project01 from './../img/projects/01.png';
+import project01Big from './../img/projects/01-big.png';
 
-import project02 from "./../img/projects/02.png";
-import project02Big from "./../img/projects/02-big.png";
+import project02 from './../img/projects/02.png';
+import project02Big from './../img/projects/02-big.png';
 
-import project03 from "./../img/projects/03.png";
-import project03Big from "./../img/projects/03-big.png";
+import project03 from './../img/projects/03.png';
+import project03Big from './../img/projects/03-big.png';
 
-import project04 from "./../img/projects/04.png";
-import project04Big from "./../img/projects/04-big.png";
+import project04 from './../img/projects/04.png';
+import project04Big from './../img/projects/04-big.png';
 
-import project05 from "./../img/projects/05.png";
-import project05Big from "./../img/projects/05-big.png";
+import project05 from './../img/projects/05.png';
+import project05Big from './../img/projects/05-big.png';
 
-import project06 from "./../img/projects/06.png";
-import project06Big from "./../img/projects/06-big.png";
+import project06 from './../img/projects/06.png';
+import project06Big from './../img/projects/06-big.png';
+
+import project07 from './../img/projects/filmoteka-small.png';
+import project07Big from './../img/projects/filmoteka-big.png';
+
+import project08 from './../img/projects/ice-cream-small.png';
+import project08Big from './../img/projects/ice-cream-big.png';
+
+import project09 from './../img/projects/your-pet-small.png';
+import project09Big from './../img/projects/your-pet-big.png';
 
 const projects = [
   {
-    title: "Rainy forest page",
-    skills: "HTML, CSS, JS",
+    title: 'Filmoteka',
+    skills: 'HTML, CSS, JS',
+    img: project07,
+    imgBig: project07Big,
+    gitHubLink: 'https://dmytro-ivanenko.github.io/Project_7_Filmoteka/#en',
+  },
+  {
+    title: 'Ice-cream landing',
+    skills: 'HTML, CSS, JS',
+    img: project08,
+    imgBig: project08Big,
+    gitHubLink: 'https://vadim-rafalskiy.github.io/Ice-cream/',
+  },
+  {
+    title: 'Find your pet',
+    skills: 'JS, NODE.JS, EXPRESS, CLOUDINARY',
+    img: project09,
+    imgBig: project09Big,
+    gitHubLink:
+      'https://dmytro-ivanenko.github.io/goit_command_project_yourPet/',
+  },
+
+  {
+    title: 'Rainy forest page',
+    skills: 'HTML, CSS, JS',
     img: project01,
     imgBig: project01Big,
-    gitHubLink: "https://vityanazarov.github.io/lens-effect-HTML-CSS/",
+    gitHubLink: 'https://vityanazarov.github.io/lens-effect-HTML-CSS/',
   },
   {
-    title: "Fairy forest",
-    skills: "HTML, CSS, JS",
+    title: 'Fairy forest',
+    skills: 'HTML, CSS, JS',
     img: project02,
     imgBig: project02Big,
-    gitHubLink: "https://vityanazarov.github.io/fairy-forest/",
+    gitHubLink: 'https://vityanazarov.github.io/fairy-forest/',
   },
   {
-    title: "Web Studio",
-    skills: "HTML, CSS, SASS, Parcel",
+    title: 'Web Studio',
+    skills: 'HTML, CSS, SASS, Parcel',
     img: project03,
     imgBig: project03Big,
-    gitHubLink: "https://vityanazarov.github.io/goit-markup-hw-08/",
+    gitHubLink: 'https://vityanazarov.github.io/goit-markup-hw-08/',
   },
   {
-    title: "Telephone book",
-    skills: "ReactJS, React Hooks, HTML",
+    title: 'Telephone book',
+    skills: 'ReactJS, React Hooks, HTML',
     img: project04,
     imgBig: project04Big,
-    gitHubLink: "https://vityanazarov.github.io/goit-react-hw-06-phonebook/",
+    gitHubLink: 'https://vityanazarov.github.io/goit-react-hw-06-phonebook/',
   },
   {
-    title: "Movie Finder",
-    skills: "React, JS, HTML, CSS",
+    title: 'Movie Finder',
+    skills: 'React, JS, HTML, CSS',
     img: project05,
     imgBig: project05Big,
-    gitHubLink: "https://vityanazarov.github.io/goit-react-hw-05-movies/",
+    gitHubLink: 'https://vityanazarov.github.io/goit-react-hw-05-movies/',
   },
   {
-    title: "Gallery",
-    skills: "JS, HTML, CSS, SimpleLightbox",
+    title: 'Gallery',
+    skills: 'JS, HTML, CSS, SimpleLightbox',
     img: project06,
     imgBig: project06Big,
-    gitHubLink: "https://vityanazarov.github.io/goit-js-hw-08/",
+    gitHubLink: 'https://vityanazarov.github.io/goit-js-hw-08/',
   },
 ];
 
